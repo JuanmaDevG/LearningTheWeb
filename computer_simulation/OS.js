66c344c652258a3fe3bb31c6499b097f8d4ab7a2
JuanmaDevG <jmgmgonzalez57@gmail.com>
2023-11-24 22:54:06 +0100

    Still making the externals of the computer

--- computer_simulation/OS.js
+++ computer_simulation/OS.js
@@ -13,5 +13,15 @@ function start_computer()
 */
 
 document.addEventListener("DOMContentLoaded", () => {
-    //Loads the ventilation holes of the computer
+    let ventilation_grill = document.getElementById("ventilation-grill");
+    const TEMPLATE_ROWS = 9, TEMPLATE_COLS = 50;
+    ventilation_grill.style.gridTemplateRows = `repeat(${TEMPLATE_ROWS}, 1fr)`;
+    ventilation_grill.style.gridTemplateColumns = `repeat(${TEMPLATE_COLS}, 1fr)`;
+    for(let i=1; i <= TEMPLATE_ROWS; i++)
+    {
+        for(let j=1; j < TEMPLATE_COLS; j++)
+        {
+            //TODO: Append the ventilation holes to the ventilation gill object
+        }
+    }
 })
