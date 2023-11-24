66999360a867e46abef89fbf15d4ba7109d605be
JuanmaDevG <jmgmgonzalez57@gmail.com>
2023-11-24 20:14:18 +0100

    Making the ventilation grill of the computer, generating the grid with JS

--- computer_simulation/OS.js
+++ computer_simulation/OS.js
@@ -4,3 +4,14 @@ function start_computer()
 {
     //Communicate with css to apply styles and animations
 }
+
+
+/* 
+    --------------------
+    -Initial operations-
+    --------------------
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    //Loads the ventilation holes of the computer
+})
