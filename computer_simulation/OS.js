764276809c3baf0acbf6f85162496c3ea26ab49c
JuanmaDevG <jmgmgonzalez57@gmail.com>
2023-11-27 19:31:18 +0100

    I have a problem with the framebuffer of the canvas because it stays unproportioned. y coord doubles x coord

--- computer_simulation/OS.js
+++ computer_simulation/OS.js
@@ -1,27 +1,44 @@
 
 
+let computer_is_running = false;
+
 function start_computer()
 {
-    //Communicate with css to apply styles and animations
+    const framebuffer = document.getElementById("computer-framebuffer");
+    const ctx = framebuffer.getContext("2d");
+
+    if(computer_is_running) //Shut down
+    {
+        //TODO: clean framebuffer and stop everything
+        computer_is_running = false;
+        return;
+    }
+    computer_is_running = true;
+
+    ctx.fillStyle = "rgb(255, 0, 0)";
+    ctx.fillRect(10, 10, 200, 100);
 }
 
 
 /* 
-    --------------------
-    -Initial operations-
-    --------------------
+    Main
 */
 
 document.addEventListener("DOMContentLoaded", () => {
     let ventilation_grill = document.getElementById("ventilation-grill");
-    const TEMPLATE_ROWS = 9, TEMPLATE_COLS = 50;
+    const TEMPLATE_ROWS = 15, TEMPLATE_COLS = 80;
     ventilation_grill.style.gridTemplateRows = `repeat(${TEMPLATE_ROWS}, 1fr)`;
     ventilation_grill.style.gridTemplateColumns = `repeat(${TEMPLATE_COLS}, 1fr)`;
+
     for(let i=1; i <= TEMPLATE_ROWS; i++)
     {
-        for(let j=1; j < TEMPLATE_COLS; j++)
+        for(let j=1; j <= TEMPLATE_COLS; j++)
         {
-            //TODO: Append the ventilation holes to the ventilation gill object
+            const ventilation_hole = document.createElement("div");
+            ventilation_hole.className = "ventilation-hole";
+            ventilation_hole.style.gridRow = `${i}`;
+            ventilation_hole.style.gridColumn = `${j}`;
+            ventilation_grill.appendChild(ventilation_hole);
         }
     }
-})
+});
