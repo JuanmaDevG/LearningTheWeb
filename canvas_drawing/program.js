262807a523e88ada3fa892ca8052c0e705aa53b0
JuanmaDevG <jmgmgonzalez57@gmail.com>
2023-12-12 01:33:33 +0100

    Some more buttons added and I keep learning the canva

--- canvas_drawing/program.js
+++ canvas_drawing/program.js
@@ -8,6 +8,14 @@ function clean(framebuffer, ctx)
     ctx.clearRect(0, 0, framebuffer.width, framebuffer.height);
 }
 
+function reset_styles(ctx)
+{
+    ctx.strokeStyle = "black";
+    ctx.fillStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.globalAlpha = 1;
+}
+
 
 /*
     Simulations to be run
@@ -36,17 +44,68 @@ function smile_face(framebuffer, ctx)
         start_angle: 0, 
         fin_angle: 2 * Math.PI
     };
+
+    //You can move the draw pointer like this or to make a lot of draw calls with [beginPath -> draw -> fill/stroke -> loop]
+
     ctx.beginPath();
-    ctx.arc(circle.x, circle.y, circle.radius, circle.start_angle, circle.fin_angle );
-    ctx.stroke();
-    ctx.closePath();
+    //Face
+    ctx.arc(circle.x, circle.y, circle.radius, circle.start_angle, circle.fin_angle);
+    //Left eye
+    ctx.moveTo(circle.x - (circle.radius / 2) + (circle.radius / 10), circle.y - (circle.radius / 2));
     ctx.arc(circle.x - (circle.radius / 2), circle.y - (circle.radius / 2), circle.radius / 10, circle.start_angle, circle.fin_angle);
-    ctx.stroke();
-    ctx.closePath();
+    //Right eye
+    ctx.moveTo(circle.x + (circle.radius / 2) + (circle.radius / 10), circle.y - (circle.radius / 2));
     ctx.arc(circle.x + (circle.radius / 2), circle.y - (circle.radius / 2), circle.radius / 10, circle.start_angle, circle.fin_angle);
-    ctx.stroke();
+    //Mouth
+    ctx.moveTo(circle.x + (circle.radius * 0.6), circle.y);
+    ctx.arc(circle.x, circle.y, circle.radius * 0.6, circle.start_angle, circle.fin_angle / 2);
     ctx.closePath();
-    //TODO: solve the bug
+    ctx.stroke();
+}
+
+function bezier_curve(framebuffer, ctx)
+{
+    clean(framebuffer, ctx);
+
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo(0, framebuffer.height / 2);
+    ctx.bezierCurveTo(
+        framebuffer.width / 4, framebuffer.height / 8, 
+        3 * (framebuffer.width / 4), 7 * (framebuffer.height / 8), 
+        framebuffer.width, framebuffer.height / 2
+    );
+    ctx.stroke();
+    reset_styles(ctx);
+}
+
+function plot_twist(framebuffer, ctx)
+{
+    clean(framebuffer, ctx);
+    
+    ctx.beginPath();
+    ctx.moveTo(75 * 6, 25 * 6);
+    ctx.quadraticCurveTo(25 * 6, 25 *  6, 25 * 6, 62.5 *6);
+    ctx.quadraticCurveTo(25 * 6, 100 * 6, 50 * 6, 100 * 6);
+    ctx.quadraticCurveTo(50 * 6, 120 * 6, 30 * 6, 125 * 6);
+    ctx.quadraticCurveTo(60 * 6, 120 * 6, 65 * 6, 100 * 6);
+    ctx.quadraticCurveTo(125 * 6, 100 *6, 125 *6, 62.5 *6);
+    ctx.quadraticCurveTo(125 * 6, 25 * 6, 75 * 6, 25 *  6);
+    ctx.stroke();
+}
+
+//Storing graphics into a Path2D object is better for performance
+function path2DRectangle(framebuffer, ctx)
+{
+    clean(framebuffer, ctx);
+    const draw_buf = new Path2D();
+
+    const px = framebuffer.width / 4, py = framebuffer.height / 4;
+    draw_buf.rect(px, py, px * 2, py * 2);
+
+    ctx.fillStyle = "rgb(120, 20, 80)";
+    ctx.fill(draw_buf);
+    reset_styles(ctx);
 }
 
 
@@ -57,7 +116,10 @@ function smile_face(framebuffer, ctx)
 const simulations = [
     { name: "Clear screen", proc: clean },
     { name: "Draw red triangle", proc: red_triangle}, 
-    { name: "Draw smiley face", proc: smile_face }
+    { name: "Draw smiley face", proc: smile_face }, 
+    { name: "Bezier curve", proc: bezier_curve }, 
+    { name: "Plot twist", proc: plot_twist }, 
+    { name: "Centered rectangle", proc: path2DRectangle}
 ];
 
 addEventListener("DOMContentLoaded", () => {
